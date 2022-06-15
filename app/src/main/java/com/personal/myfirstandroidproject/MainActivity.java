f61145439619c28b3b526970cffde22af260c84d
Luis Carlos Guevara <luisc.guevaram@ecci.edu.co>
2022-06-15 11:19:59 -0500

    Es mi primer proyecto de Android para revision

---
+++ app/src/main/java/com/personal/myfirstandroidproject/MainActivity.java
@@ -0,0 +1,23 @@
+package com.personal.myfirstandroidproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+          //      WindowManager.LayoutParams.FLAG_FULLSCREEN);
+                setContentView(R.layout.activity_main);
+
+                getSupportActionBar().hide();
+
+    }
+}
